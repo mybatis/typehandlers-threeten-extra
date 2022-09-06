0f37d5a5a2728ef87a808cbd72bb562059e5baa5
Jeremy Landis <jeremylandis@hotmail.com>
2022-09-05 21:16:32 -0400

    [pom] Skip byte code enforcement for threeten extra as expected higher

--- src/test/java/org/mybatis/typehandlers/threetenextra/MinutesTypeHandlerTest.java
+++ src/test/java/org/mybatis/typehandlers/threetenextra/MinutesTypeHandlerTest.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2017-2021 the original author or authors.
+ *    Copyright 2017-2022 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@ import org.junit.jupiter.api.Test;
 import org.threeten.extra.Minutes;
 
 /**
- *
  * @author Björn Raupach
  */
 public class MinutesTypeHandlerTest extends BaseTypeHandlerTest {
